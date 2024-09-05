efc7c63d0c4779b207711fe73ffb6b889c865490
DimaRyndya <dmitriy.ryndya@gmail.com>
2024-09-05 09:42:50 +0300

    Initial Commit

---
+++ NYTimesNews/NYTimesNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYTimesNewsApp.swift
+//  NYTimesNews
+//
+//  Created by Dmitriy Ryndya on 05.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct NYTimesNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
